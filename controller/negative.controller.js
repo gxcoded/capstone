5416bbfe5e7dbdeea7b76e1503515b95245dd279
gxcoded <116895533+gxcoded@users.noreply.github.com>
2023-01-05 10:16:15 +0800

    Update negative.controller.js

--- controller/negative.controller.js
+++ controller/negative.controller.js
@@ -1,5 +1,32 @@
 const Negative = require("../model/negative.model");
 
+exports.manualNegative = async (image, body) => {
+  let success = false;
+
+  const newReport = {
+    campus: body.campus,
+    accountOwner: body.accountOwner,
+    testType: body.testType,
+    dateTested: body.dateTested,
+    dateSent: body.dateSent,
+    resultDate: body.resultDate,
+    imgProof: image,
+  };
+
+  const newNegative = new Negative(newReport);
+
+  await newNegative
+    .save()
+    .then(async (result) => {
+      success = true;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return success;
+};
+
 exports.reportNegative = async (image, body) => {
   let success = false;
 
